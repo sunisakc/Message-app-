c9b152ce734f3f4927485229f1250baa7f210c3d
sunisakc <sunisa.k@gmail.com>
2018-11-05 19:41:18 +0100

    add data

--- server.js
+++ server.js
@@ -3,28 +3,44 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+var dbUrl = 'mongodb://user:admin1@ds151453.mlab.com:51453/learnnode'
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) =>{
-    
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
 })
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if (err)
+            sendStatus(500)
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+
+})
+
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
